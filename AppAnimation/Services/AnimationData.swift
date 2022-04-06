4ec8b1aeae4e7b5105ce116300c696ea9a676364
maximsamus <maximsamus1@gmail.com>
2022-04-06 15:08:51 +0300

    Structure

---
+++ AppAnimation/Services/AnimationData.swift
@@ -0,0 +1,8 @@
+//
+//  AnimationData.swift
+//  AppAnimation
+//
+//  Created by Максим Самусь on 06.04.2022.
+//
+
+import Foundation
---
+++ AppAnimation/ViewControllers/Models/Animation.swift
@@ -0,0 +1,7 @@
+//
+//  Animation.swift
+//  AppAnimation
+//
+//  Created by Максим Самусь on 06.04.2022.
+//
+
